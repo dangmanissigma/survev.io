d7f9ff04d2e2636a3a910b0e1d68f9bc5a37c7c7
dangmanissigma <ggela146@gmail.com>
2026-09-08 08:23:06 +0000

    Revert "Fix local test bot WebSocket URL"
    This reverts commit 2f42f958920deba7c2afbb15c8abaf4feb3eaff4.

--- survev-master-updated/survev-master/server/src/aimbotTestBot.ts
+++ survev-master-updated/survev-master/server/src/aimbotTestBot.ts
@@ -51,14 +51,7 @@ class DummyBot {
     constructor(match: FindGameMatchData) {
         // NOTE: upstream now returns full connection URLs directly (res.urls),
         // instead of an addr you had to build a /play?gameId=... path from.
-        const returnedUrl = match.urls[0];
-        const wsUrl = returnedUrl.startsWith("/")
-            ? (() => {
-                const localUrl = new URL(returnedUrl, "http://localhost");
-                const port = localUrl.searchParams.get("port");
-                return `ws://localhost:${port ?? "80"}${localUrl.pathname}`;
-            })()
-            : returnedUrl.replace(/^http/, "ws");
+        const wsUrl = match.urls[0];
         this.joinToken = match.joinToken;
 
         console.log("[DummyBot] connecting to", wsUrl);
