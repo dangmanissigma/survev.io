488ed041728f4e8e415f01c1a3423c92c0979b63
dangmanissigma <ggela146@gmail.com>
2026-09-08 08:23:06 +0000

    Revert "Add sparse tree test arena"
    This reverts commit 7e8049501b8bb63dd039e0807664560cc54984e8.

--- survev-master-updated/survev-master/client/src/config.ts
+++ survev-master-updated/survev-master/client/src/config.ts
@@ -106,7 +106,7 @@ const defaultConfig = {
     profile: null as { slug: string } | null,
     playerName: "",
     region: "na",
-    gameModeIdx: 3,
+    gameModeIdx: 2,
     teamAutoFill: true,
     language: "en" as Locale,
     prerollGamesPlayed: 0,
